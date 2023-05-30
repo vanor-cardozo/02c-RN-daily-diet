96a3897d612164cc90f2e7076e5886c16ae560df
vanor-cardozo <vanor.fc@gmail.com>
2023-05-30 16:29:36 -0300

    feat: Tela para cadastrar uma nova refeição

---
+++ src/screens/Meal/index.tsx
@@ -0,0 +1,176 @@
+import { useState } from "react";
+import { TouchableOpacity } from "react-native";
+
+import { ScreenBackGround } from "@components/ScreenBackground";
+
+import {
+  ContainerInline,
+  ContainerInColumn,
+  FormContainer,
+  Input,
+  InputLabel,
+  ButtonGreen,
+  ButtonRed,
+  Text,
+  InputsContainer,
+} from "./styles";
+
+import DateTimePicker, {
+  DateTimePickerEvent,
+} from "@react-native-community/datetimepicker";
+
+import theme from "@theme/index";
+import { Circle } from "phosphor-react-native";
+import { Button } from "@components/Button";
+import { useNavigation } from "@react-navigation/native";
+
+export function Meal() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [showTimePicker, setShowTimePicker] = useState(false);
+
+  const [mealName, setMealName] = useState("");
+  const [mealDescription, setMealDescription] = useState("");
+  const [mealDiet, setMealDiet] = useState(false);
+
+  const [buttonGreenPressed, setButtonGreenPressed] = useState(false);
+  const [buttonRedPressed, setButtonRedPressed] = useState(false);
+
+  const { navigate } = useNavigation();
+
+  function handleDateChange(
+    event: DateTimePickerEvent,
+    date?: Date | undefined
+  ) {
+    setShowDatePicker(false);
+    if (event.type === "set" && date) {
+      setSelectedDate(date);
+    }
+  }
+
+  function handleTimeChange(
+    event: DateTimePickerEvent,
+    date?: Date | undefined
+  ) {
+    setShowTimePicker(false);
+    if (event.type === "set" && date) {
+      setSelectedDate(date);
+    }
+  }
+
+  function handleSubmit() {
+    const newMeal = {
+      date: selectedDate,
+      name: mealName.trimStart(),
+      description: mealDescription.trimStart(),
+      diet: mealDiet,
+    };
+
+    const areAllFieldsFilled = Object.values(newMeal).every(
+      (value) => value !== undefined && value !== ""
+    );
+
+    if (areAllFieldsFilled) {
+      if (newMeal.diet) {
+        navigate("dietFeedback");
+      } else {
+        navigate("noDietFeedback");
+      }
+    } else {
+      console.log("preencher todos os campos");
+    }
+  }
+
+  function handleGreenButton() {
+    setButtonGreenPressed(true);
+    setButtonRedPressed(false);
+    setMealDiet(true);
+  }
+
+  function handleRedButton() {
+    setButtonRedPressed(true);
+    setButtonGreenPressed(false);
+    setMealDiet(false);
+  }
+
+  return (
+    <ScreenBackGround title="Refeição">
+      <FormContainer>
+        <InputsContainer>
+          <InputLabel>Nome</InputLabel>
+          <Input value={mealName} onChangeText={setMealName} />
+
+          <InputLabel>Descrição</InputLabel>
+          <Input
+            multiline
+            textAlignVertical="top"
+            numberOfLines={5}
+            value={mealDescription}
+            onChangeText={setMealDescription}
+          />
+
+          <ContainerInline>
+            <ContainerInColumn>
+              <InputLabel>Data</InputLabel>
+              <TouchableOpacity onPressIn={() => setShowDatePicker(true)}>
+                <Input
+                  value={selectedDate.toLocaleDateString()}
+                  editable={false}
+                />
+              </TouchableOpacity>
+
+              {showDatePicker && (
+                <DateTimePicker
+                  value={selectedDate}
+                  mode="date"
+                  display="calendar"
+                  onChange={handleDateChange}
+                />
+              )}
+            </ContainerInColumn>
+
+            <ContainerInColumn>
+              <InputLabel>Hora</InputLabel>
+              <TouchableOpacity onPressIn={() => setShowTimePicker(true)}>
+                <Input
+                  value={selectedDate.toLocaleTimeString().slice(0, 5)}
+                  editable={false}
+                />
+              </TouchableOpacity>
+
+              {showTimePicker && (
+                <DateTimePicker
+                  value={selectedDate}
+                  mode="time"
+                  display="clock"
+                  onChange={handleTimeChange}
+                />
+              )}
+            </ContainerInColumn>
+          </ContainerInline>
+
+          <ContainerInline>
+            <ButtonGreen
+              isPressed={buttonGreenPressed}
+              onPress={handleGreenButton}
+            >
+              <Circle size={10} weight="fill" color={theme.COLORS.GREEN_DARK} />
+              <Text>Sim</Text>
+            </ButtonGreen>
+
+            <ButtonRed isPressed={buttonRedPressed} onPress={handleRedButton}>
+              <Circle size={10} weight="fill" color={theme.COLORS.RED_DARK} />
+              <Text>Não</Text>
+            </ButtonRed>
+          </ContainerInline>
+        </InputsContainer>
+
+        <Button
+          type="DARK"
+          buttonName="Cadastrar refeição"
+          onPress={handleSubmit}
+        />
+      </FormContainer>
+    </ScreenBackGround>
+  );
+}
